136d0cce5d30747adfd9af962e23f095e051b1d8
Jevin Sweval <jevinsweval@gmail.com>
2022-10-26 18:46:30 -0400

    wip

--- python/aarch64_experimental_disasm/mra_encoding_xml.py
+++ python/aarch64_experimental_disasm/mra_encoding_xml.py
@@ -1,22 +1,86 @@
 import re
 from pathlib import Path
-from typing import Union
+from typing import Optional, Union
 
 from attrs import define
 from lxml import objectify
+from rich import print
+
+
+@define
+class Constraint:
+    val: int
+    ne: bool
+
+
+@define
+class Field:
+    pos: int
+    sz: int
+    constraint: Optional[Constraint]
+    name: Optional[str]
 
 
 @define
 class Encoding:
     mnemonic: str
     name: str
-    pos_bitmask: int
-    pos_bitpattern: int
-    neg_bitmask: int
-    neg_bitpattern: int
+    path: Path
+    pos_mask: int
+    pos_val: int
+    neg_mask: int
+    neg_val: int
+    fields: tuple[Field]
 
 
 BINSTR_RE = re.compile("[01]+")
+NE_BINSTR_RE = re.compile("!= [01]+")
+
+
+def bitmask(pos: int, nbits: int) -> int:
+    hi_mask = (1 << (pos + nbits)) - 1
+    lo_mask = (1 << pos) - 1
+    return hi_mask ^ lo_mask
+
+
+def parse_boxes(box) -> tuple[Field]:
+    return ()
+
+
+def parse_fields(fields: tuple[Field]) -> tuple[int, int, int, int]:
+    pos_mask, pos_val, neg_mask, neg_val = 0, 0, 0, 0
+    for f in fields:
+        if f.constraint is None:
+            continue
+        c = f.constraint
+        if not c.neg:
+            pos_mask |= bitmask(f.pos, f.sz)
+            pos_val |= c.val << f.pos
+        else:
+            neg_mask |= bitmask(f.pos, f.sz)
+            neg_val |= c.val << f.pos
+    return pos_mask, pos_val, neg_mask, neg_val
+
+
+def parse_instruction_xml(xml_instsect_file: Path) -> tuple[Encoding]:
+    encodings = []
+    tree = objectify.parse(str(xml_instsect_file))
+    root = tree.getroot()
+    iclasses = root.classes.iclass
+    for iclass in iclasses:
+        path = iclass.regdiagram.attrib["psname"]
+        path.replace("aarch64/instrs", "aarch64")
+        path = Path(path).parent
+        name = iclass.encoding.attrib["name"]
+        mnemonic = next(
+            filter(lambda dv: dv.attrib["key"] == "mnemonic", iclass.docvars.docvar)
+        ).attrib["value"]
+        boxes = iclass.regdiagram.box
+        fields = parse_boxes(boxes)
+        pos_mask, pos_val, neg_mask, neg_val = parse_fields(fields)
+        enc = Encoding(mnemonic, name, path, pos_mask, pos_val, neg_mask, neg_val, fields)
+        print(iclass)
+    return tuple(*encodings)
 
 
 def c_val(box) -> Union[None, int]:
@@ -30,8 +94,8 @@ def c_val(box) -> Union[None, int]:
     return int(bitstr, 2)
 
 
-def parse_encodings_xml(xml_dir: str) -> list[Encoding]:
-    enc_xml_path = Path(xml_dir) / "encodingindex.xml"
+def parse_encodings_xml(xml_dir: Path) -> tuple[Encoding]:
+    enc_xml_path = xml_dir / "encodingindex.xml"
     tree = objectify.parse(str(enc_xml_path))
     root = tree.getroot()
     iclass_sects = root.iclass_sect
@@ -51,4 +115,4 @@ def parse_encodings_xml(xml_dir: str) -> list[Encoding]:
 
     encodings = []
 
-    return encodings
+    return tuple(*encodings)
--- python/aarch64_experimental_disasm/tools/dump_instrs.py
+++ python/aarch64_experimental_disasm/tools/dump_instrs.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 
 import argparse
+from pathlib import Path
 
 from aarch64_experimental_disasm import mra_encoding_xml
+from rich import print
 
 
 def real_main(args):
-    encs = mra_encoding_xml.parse_encodings_xml(args.xml_dir)
-    print(encs)
+    if args.xml_dir is not None:
+        encs = mra_encoding_xml.parse_encodings_xml(args.xml_dir)
+        print(encs)
+    elif args.xml_inst is not None:
+        encs = mra_encoding_xml.parse_instruction_xml(args.xml_inst)
+        print(encs)
 
 
 def get_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="aarch64-dump-instrs")
-    parser.add_argument("-x", "--xml-dir", required=True, help="MRA XML directory")
+    parser.add_argument("-x", "--xml-dir", type=Path, help="MRA XML directory")
+    parser.add_argument("-i", "--xml-inst", type=Path, help="MRA XML instruction file")
     return parser
 
 
